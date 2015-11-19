dfbbe322bc03490fcd9fb70eb10c916439a8c8ce
thibault.notargiacomo <thibault.notargiacomo@thalesgroup.com>
2015-11-19 16:34:26 +0100

    Debut implémentation backprop : OK en non régularisé

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -38,7 +38,50 @@ Theta2_grad = zeros(size(Theta2));
 %         variable J. After implementing Part 1, you can verify that your
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
-%
+
+%Layer 1
+%Add bias to original dataset
+a1=[ones(m,1) X];
+
+%Layer 2
+%Now apply a1*Theta1
+z2 = a1*Theta1.';
+%Apply sigmoid
+a2 = sigmoid(z2);
+a2 = [ones(m,1) a2];
+
+%Layer 3
+z3 = a2*Theta2.';
+a3 = sigmoid(z3);
+
+%Now the output layer is filled, we must compute the cost
+
+%First step, put y into a handy form
+nbClass = size(Theta2,1);
+order = [1:nbClass];
+order = repmat(order,m,1);
+% 1 2 3
+% 1 2 3
+% 1 2 3
+% 1 2 3
+
+y=repmat(y,1,nbClass);
+% 1 1 1
+% 3 3 3
+% 1 1 1
+% 2 2 2
+
+y=(order==y);
+% 1 0 0
+% 0 0 1
+% 1 0 0
+% 0 1 0
+
+%Apply cost formula
+costMatrix = -y.*log(a3) - (ones(size(y))-y).*log(ones(size(a3))-a3);
+J=(1/m)*sum(sum(costMatrix)) ;
+
+
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
@@ -53,7 +96,19 @@ Theta2_grad = zeros(size(Theta2));
 %         Hint: We recommend implementing backpropagation using a for-loop
 %               over the training examples if you are implementing it for the 
 %               first time.
-%
+
+%Compute the delta of the last layer
+delta3 = a3-y;
+
+%Compute the delta of layer 2
+delta2= delta3 * Theta2 .* (a2.*(ones(size(a2))-a2));
+%There should be no delta on the bias unit
+delta2 = delta2(:,2:size(delta2,2));
+
+%Compute gradient
+Theta1_grad = (1/m) * delta2.'*a1;
+Theta2_grad = (1/m) * delta3.'*a2;
+
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
@@ -62,20 +117,9 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+regularization = norm(Theta1(:,1:size(Theta1,2)))^2 + ...
+		norm(Theta2(:,1:size(Theta2,2)))^2 ;
+%J = J + (lambda/m)*regularization ;
 
 
 
